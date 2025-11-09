4fef8c68361299ab8de55f2f04074a5cad9e2fd0
huynhgiaau27112005 <huynhgiaau27112005@gmail.com>
2025-11-09 10:02:47 +0700

    init: pages

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,23 +4,33 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 
 function page() {
-    const [message, setMessage] = useState("Loading");
+  const [message, setMessage] = useState("Loading");
 
-    useEffect(() => {
-        fetch("http://localhost:8080/").then(
-            response => response.json()
-        ).then (
-            data => {
-                console.log(data);
-                setMessage(data.message);
-            }
-        )
-    }, [])
-    return (
-        <div>
-            {message}
-        </div>
+  useEffect(() => {
+    fetch("http://localhost:8080/").then(
+      response => response.json()
+    ).then(
+      data => {
+        console.log(data);
+        setMessage(data.message);
+      }
     )
+  }, [])
+  return (
+    <div>
+      {message}
+    </div>
+  )
 }
 
-export default page
+export default page;
+// "/category/[:category_slugs]/product/[:product_slug]"
+// "/user/info"
+// "/user/rating"
+// "/user/favourite_products"
+// "/user/bidding_products"
+// "/user/winning_products"
+// "/user/seller_role"
+// "/user/selling_products"
+// "/user/sold_products"
+
