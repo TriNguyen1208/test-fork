66fb35a6db3d6546f6043028757c20796bbd189a
huynhgiaau27112005 <huynhgiaau27112005@gmail.com>
2025-11-28 21:44:24 +0700

    fix: auth

--- client/store/auth.store.ts
+++ client/store/auth.store.ts
@@ -1,28 +1,31 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 interface User {
-    id: string;
-    email: string;
+  id: string;
+  email: string;
 }
 
 interface AuthState {
-    user: User | null;
-    setUser: (user: User | null) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
 
-    mockLogin: () => void;
-    mockLogout: () => void;
+  mockLogin: () => void;
+  mockLogout: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set, get) => ({
-    user: null,
-    setUser: (user) => set({ user }),
+  user: {
+    id: "1",
+    email: "mock@example.com",
+  },
+  setUser: (user) => set({ user }),
 
-    mockLogin: () =>
-        set({
-            user: {
-                id: "1",
-                email: "mock@example.com",
-            },
-        }),
-    mockLogout: () => set({ user: null })
-}))
+  mockLogin: () =>
+    set({
+      user: {
+        id: "1",
+        email: "mock@example.com",
+      },
+    }),
+  mockLogout: () => set({ user: null }),
+}));
