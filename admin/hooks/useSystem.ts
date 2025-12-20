8fecbad440a0e13566b3a166e40265486cbee415
Donavfulish <dvha23@clc.fitus.edu.vn>
2025-12-20 13:52:23 +0700

    feat(admin-systemhook): change toast to toastify

--- admin/hooks/useSystem.ts
+++ admin/hooks/useSystem.ts
@@ -1,7 +1,8 @@
 import { SystemService } from "@/services/SystemService";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { STALE_10_MIN } from "@/config/query.config";
-import toast from "react-hot-toast";
+import { toast } from "react-toastify"
+
 class SystemHook {
     static useGetProductRenewTime() {
         return useQuery({
